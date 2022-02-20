20c62e6244974dff920e95a9a64724f5d7e4532e
ElectricR <chulkovroma@mail.ru>
2022-02-20 03:38:22 +0300

    Test linting

--- test/e2e/e2e.py
+++ test/e2e/e2e.py
@@ -5,7 +5,9 @@ import os
 
 class SmokeTest(unittest.TestCase):
     def check(self, test_input, correct_answer):
-        proc = subprocess.Popen(["./build/topsh"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(["./build/topsh"],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE)
         out = proc.communicate(test_input.encode())[0].decode("UTF-8")
 
         self.assertEqual(out, correct_answer)
@@ -22,4 +24,3 @@ class SmokeTest(unittest.TestCase):
 
     def test_echo_after_exit(self):
         self.check("exit\necho Hello", "")
-
