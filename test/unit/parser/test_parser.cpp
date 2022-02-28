04e5776f1a4110ad8421c92aa2d92cec56ae4d34
ElectricR <chulkovroma@mail.ru>
2022-02-28 15:17:13 +0300

    Added Parser unit tests

---
+++ test/unit/parser/test_parser.cpp
@@ -0,0 +1,35 @@
+#include "interpreter/parser/parser.h"
+
+#include <gtest/gtest.h>
+
+TEST(BasicAssertions, ParserGroup) {
+    auto answer = interpreter::internal::Command{.name = "aoeu", .args = ""};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("aoeu"));
+
+    answer = interpreter::internal::Command{.name = "cat", .args = ""};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("cat"));
+
+    answer = interpreter::internal::Command{.name = "exit", .args = ""};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("exit"));
+
+    answer = interpreter::internal::Command{.name = "cat", .args = "file"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("cat file"));
+
+    answer = interpreter::internal::Command{.name = "cat", .args = "exit"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("cat exit"));
+
+    answer = interpreter::internal::Command{.name = "exit", .args = "cat"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("exit cat"));
+
+    answer = interpreter::internal::Command{.name = "cat", .args = "cat cat cat exit"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("cat cat cat cat exit"));
+
+    answer = interpreter::internal::Command{.name = "echo", .args = "lol"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("echo lol"));
+
+    answer = interpreter::internal::Command{.name = "wc", .args = "lol kek"};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("wc lol kek"));
+
+    answer = interpreter::internal::Command{.name = "pwd", .args = ""};
+    EXPECT_EQ(answer, interpreter::parser::Parser().Parse("pwd"));
+}
